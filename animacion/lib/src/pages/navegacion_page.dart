e2ebe1d033cf101d067a42dd1caae7d4767ead02
Nicolas <na796840@gmail.com>
2023-02-02 15:02:58 -0500

    animacion

---
+++ animacion/lib/src/pages/navegacion_page.dart
@@ -0,0 +1,115 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:provider/provider.dart';
+import 'package:animate_do/animate_do.dart';
+
+class NavagacionPage extends StatelessWidget {
+  const NavagacionPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (_) => new _NotificationModel(),
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.red,
+          title: const Text("Notifications Page"),
+        ),
+        floatingActionButton: const BotonFlotante(),
+        bottomNavigationBar: const BottomNavigation(),
+      ),
+    );
+  }
+}
+
+class BottomNavigation extends StatelessWidget {
+  const BottomNavigation({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final int numero = Provider.of<_NotificationModel>(context).numero;
+    return BottomNavigationBar(
+      currentIndex: 0,
+      selectedItemColor: Colors.red,
+      items: [
+        const BottomNavigationBarItem(
+            label: ("Bones"), icon: FaIcon(FontAwesomeIcons.bone)),
+        BottomNavigationBarItem(
+            label: ("Notificaciones"),
+            icon: Stack(
+              children: <Widget>[
+                const FaIcon(FontAwesomeIcons.bell),
+                Positioned(
+                  top: 0.0,
+                  right: 0.0,
+                  child: BounceInDown(
+                    from: 10,
+                    animate: (numero > 0) ? true : false,
+                    child: Bounce(
+                      from: 10,
+                      controller: (controller) =>
+                          Provider.of<_NotificationModel>(context)
+                              .bounceController = controller,
+                      child: Container(
+                        alignment: Alignment.center,
+                        width: 12,
+                        height: 12,
+                        decoration: const BoxDecoration(
+                            color: Colors.redAccent, shape: BoxShape.circle),
+                        child: Text(
+                          "$numero",
+                          style:
+                              const TextStyle(color: Colors.white, fontSize: 7),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            )),
+        const BottomNavigationBarItem(
+            label: ("My Dog"), icon: FaIcon(FontAwesomeIcons.dog)),
+      ],
+    );
+  }
+}
+
+class BotonFlotante extends StatelessWidget {
+  const BotonFlotante({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return FloatingActionButton(
+        backgroundColor: Colors.red,
+        onPressed: () {
+          final notiModel =
+              Provider.of<_NotificationModel>(context, listen: false);
+
+          int numero = notiModel.numero;
+          numero++;
+          notiModel.numero = numero;
+          if (numero >= 2) {
+            final controller =
+                Provider.of<_NotificationModel>(context).bounceController;
+            controller.forward(from: 0.0);
+          }
+        },
+        child: const FaIcon(FontAwesomeIcons.play));
+  }
+}
+
+class _NotificationModel extends ChangeNotifier {
+  int _numero = 0;
+
+  late AnimationController _bounceController;
+
+  int get numero => _numero;
+  set numero(int valor) {
+    notifyListeners();
+  }
+
+  AnimationController get bounceController => this._bounceController;
+  set bounceController(AnimationController controller) {
+    this._bounceController = controller;
+  }
+}
---
+++ animacion/lib/src/pages/pagina1_page.dart
@@ -0,0 +1,87 @@
+import 'package:animacion/src/pages/twitter_page.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:animate_do/animate_do.dart';
+import 'package:animacion/src/pages/navegacion_page.dart';
+
+class Pagina1Page extends StatelessWidget {
+  const Pagina1Page({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: FadeIn(
+            duration: const Duration(milliseconds: 500),
+            child: const Text("Animate_do")),
+        actions: <Widget>[
+          IconButton(
+              onPressed: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (BuildContext context) =>
+                            const TwitterPage()));
+              },
+              icon: const FaIcon(FontAwesomeIcons.twitter)),
+          SlideInLeft(
+            from: 100,
+            child: IconButton(
+                onPressed: () {
+                  Navigator.push(
+                      context,
+                      CupertinoPageRoute(
+                          builder: (BuildContext context) =>
+                              const Pagina1Page()));
+                },
+                icon: const Icon(Icons.navigate_next)),
+          ),
+        ],
+      ),
+      floatingActionButton: ElasticInRight(
+        child: FloatingActionButton(
+          child: const FaIcon(FontAwesomeIcons.play),
+          onPressed: () {
+            Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (BuildContext context) => const NavagacionPage()));
+          },
+        ),
+      ),
+      body: Center(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          ElasticIn(
+              delay: const Duration(milliseconds: 1100),
+              child:
+                  const Icon(Icons.new_releases, color: Colors.blue, size: 40)),
+          FadeInDown(
+            delay: const Duration(milliseconds: 2000),
+            child: const Text(
+              "Titulo",
+              style: TextStyle(fontSize: 40, fontWeight: FontWeight.w200),
+            ),
+          ),
+          FadeInDown(
+            delay: const Duration(milliseconds: 800),
+            child: const Text(
+              "Soy un texto pequeño",
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.w200),
+            ),
+          ),
+          FadeInLeft(
+            delay: const Duration(milliseconds: 1100),
+            child: Container(
+              width: 220,
+              height: 2,
+              color: Colors.blue,
+            ),
+          )
+        ],
+      )),
+    );
+  }
+}
